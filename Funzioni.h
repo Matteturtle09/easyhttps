e8922135c7c45564ed5fbf6a64b4fa60b9417782
Matteturtle09 <78372611+Matteturtle09@users.noreply.github.com>
2022-07-18 14:36:55 +0200

    Update Funzioni.h

--- Funzioni.h
+++ Funzioni.h
@@ -6,5 +6,5 @@
 void init_wifi(const char* ssid, const char* password, int baudrate); // connette l'esp al wifi + debug
 String https_request(String url, char* host, int httpsPort, char* fingerprint, WiFiClientSecure client); // richiesta https sicura con verifica del certificato
 String https_request_ins(String url, char* host, int httpsPort, WiFiClientSecure client);
-/* funzioni ethereum */
+
 #endif funzioni_h
